784fefd99b45eef7651b77623d707acbd123a68e
Diego Miret <dmirethalffter@gmail.com>
2026-04-30 17:28:41 +0200

    FixAmplifyError

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://tepeuve.es',
   output: 'static',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false, // usamos nuestros estilos base
-    }),
-  ],
 });
