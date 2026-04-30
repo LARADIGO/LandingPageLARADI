a3c195fed9bb94066e694a50ba1b93dc7fc61441
Diego Miret <dmirethalffter@gmail.com>
2026-04-30 17:24:34 +0200

    bugsFixed

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://https://tepeuve.es', // cámbialo cuando tengas dominio
+  site: 'https://tepeuve.es',
   output: 'static',
   integrations: [
     tailwind({
